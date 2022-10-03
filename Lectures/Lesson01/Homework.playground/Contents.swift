ca651c93bcd6207e9c005eca703811f87ab6507f
kriperi <69962585+kriperi@users.noreply.github.com>
2022-10-03 09:53:03 +0200

    Update Contents.swift

--- Lectures/Lesson01/Homework.playground/Contents.swift
+++ Lectures/Lesson01/Homework.playground/Contents.swift
@@ -11,9 +11,12 @@ import UIKit
 - Try to set the value to something else.
 - Why did you get an error message now?
 - Change the code so that you can change the value without getting an error message
+ 
+ Because the variable is constant, it cannot change the type
 
 */
-
+var swift: String = "Swift is cool!"
+swift = "hehe"
 
 
 /*:
@@ -21,7 +24,10 @@ import UIKit
 
 Create a constant with an explicit String type and the value "iOS FTW" - Then remove the type. Why is this possible?
 
+It is possible because we don´t always have to declare the ype of variable.
 */
+
+let word = "iOS FTW"
 
 
 /*:
@@ -32,13 +38,21 @@ Create a constant with an explicit String type and the value "iOS FTW" - Then re
 
 */
 
+let number = 23
+let anotherWord = "Hello"
+let numberAndWord = "\(anotherWord) \(number)"
 
+print(numberAndWord)
 /*:
  ## Task #4
 
 Create an expression with string interpolation where you calculate the sum of 5 * 120 as part of the interpolation, print it using print()
 
 */
+let n = 5
+let x = 120
+let sum = "\(n * x)"
+print(sum)
 
 
 /*:
@@ -47,6 +61,9 @@ Create an expression with string interpolation where you calculate the sum of 5 
 Compare two strings and print "they are equal", if they are exactly the same, or "they are different" if they are different
 
 */
+let c = "Hey"
+let z = "Hey"
+print (c == z ? "They are equal" : "They are the different")
 
 
 /*:
@@ -55,15 +72,14 @@ Compare two strings and print "they are equal", if they are exactly the same, or
 Create a tuple with the content (5600, “iOS programming”). Then use print to print “PG5600 - iOS Programming”
  
 */
-
-
+let program = (code: 5600, message: "iOS programming")
+print("PG\(program.code) - \(program.message)")
 
 /*:
  ## Task #7
 
  Create a loop where you print each word in an array on its own line
  Then create a loop that prints each letter of each word on a new line
-
 
 Expected result
 */
@@ -84,14 +100,37 @@ Expected result
 //:    o
 //:    n
 
+let rocket = ["Falcon 9", "Dragon"]
+let t = "Hello World"
+var i = 0
+for word in rocket {
+    print(word)
+}
+
+
+for char in rocket {
+    for letter in char {
+        print(letter)
+    }
+}
+
+
 /*:
  ## Task #8
 
 Create a dictionary with 5 key-value pairs, loop over it and print key and value with print
 
 */
+let dictionary = ["Swift" : "Awesome",
+                  "Programing" : "Also Cool",
+                  "Boobs" : "Yes",
+                  "Ass" : "Also yes",
+                  "Hello" : "World"]
 
-
+for pair in dictionary {
+    print("\(pair.key)? \(pair.value)")
+    
+}
 
 /*:
  ## Task #9
